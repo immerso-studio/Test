df8173e3b8bff6c058e0becbaa19c349563ea108
paolo-immerso <info@immersostudio.com>
2025-07-30 18:19:37 +0200

    test6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,5 @@
 import Scene from '../components/scene'
 
 export default function Home() {
-  return (
-    <main style={{ width: '100vw', height: '100vh' }}>
-      <Scene />
-    </main>
-  )
+  return <Scene />
 }
